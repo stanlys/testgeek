10c35a8b33bace05b409267209f05961bd335d37
stanlys <stanlys@yandex.ru>
2016-08-08 22:34:59 +0300

    первые 3 урока

---
+++ ViewController2.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController2.swift
+//  test_Geek
+//
+//  Created by iMac on 08.08.16.
+//  Copyright © 2016 iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("ok2")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ test_Geek/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test_Geek
+//
+//  Created by iMac on 08.08.16.
+//  Copyright © 2016 iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("ok1")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
